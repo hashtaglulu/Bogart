fa495a1a22e5810179b255933003b2594d797867
Laura <n.laura96@yahoo.com>
2020-10-29 01:04:48 +0200

    swipeable items

--- src/app/pubs/offers/offers.page.ts
+++ src/app/pubs/offers/offers.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
@@ -10,10 +12,19 @@ import { Place } from '../place.model';
 export class OffersPage implements OnInit {
   offers: Place[];
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService, private router: Router) { }
 
   ngOnInit() {
     this.offers = this.placesService.places;
   }
 
+  onEdit(offerId: string, slidingItem: IonItemSliding){
+    slidingItem.close();
+    this.router.navigate(['/', 'pubs', 'tabs', 'offers', 'edit', offerId]);
+    console.log("edit item ", offerId);
+  }
+
+  onDelete(offerId: string, slidingItem: IonItemSliding){
+    console.log("delete item", offerId);
+  }
 }
