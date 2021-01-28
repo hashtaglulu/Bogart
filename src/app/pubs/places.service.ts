195140aeb65252a2bf631caf93672b6020db2df7
Laura <n.laura96@yahoo.com>
2021-01-28 13:46:46 +0200

    added places and booking services

--- src/app/pubs/places.service.ts
+++ src/app/pubs/places.service.ts
@@ -1,66 +1,54 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
 import { Place } from './place.model';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
-  private _places: Place[] = [
-    new Place(
-      'p1',
-      'Scart',
-      'cool place, might come back later',
-      'https://d2fdt3nym3n14p.cloudfront.net/venue/2107/gallery/5309/conversions/scart-loc-lejer-13866-big.jpg',
-      ['luni-vineri: 09:00-23:00', 'sambata: 11:00-23:00', 'duminica: 14:00-23:00'],
-      new Date(),
-      new Date()
-    ),
-    new Place(
-      'p2',
-      '80`sPub',
-      'same',
-      'https://timisoara.fest.ro/files/places/8/image_884_2_preview.jpg',
-      ['luni-joi: 10:00-02:00', 'vineri: 10:00-05:00' ,'sambata: 10:00-05:00', 'duminica: 10:00-02:00'],
-      new Date(),
-      new Date()
-    ),
-    new Place(
-      'p3',
-      'D`arc',
-      'also on lake',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcScfa8GR2gbK3gczQjCz0V47zue29V0mHZ5Qg&usqp=CAU',
-      ['luni-joi: 10:00-02:00', 'vineri: 10:00-05:00' ,'sambata: 10:00-05:00', 'duminica: 10:00-02:00'],
-      new Date(),
-      new Date()
-  ),
-  new Place(
-    'p4',
-    'Aethernative',
-    'downstairs',
-    'https://www.thebohoguide.com/wp-content/uploads/job-manager-uploads/main_image/2017/03/boho1.jpg',
-    ['luni-joi: 10:00-02:00', 'vineri: 10:00-05:00' ,'sambata: 10:00-05:00', 'duminica: 10:00-02:00'],
-    new Date(),
-    new Date()
-  ),
-  new Place(
-    'p5',
-    'Cuib D`Arte',
-    'upstairs',
-    'https://media-cdn.tripadvisor.com/media/photo-s/19/ed/90/90/img-20191104-220623-373.jpg',
-    ['luni-joi: 10:00-02:00', 'vineri: 10:00-05:00' ,'sambata: 10:00-05:00', 'duminica: 10:00-02:00'],
-    new Date(),
-    new Date()
-  ),
+  allPlaces: BehaviorSubject<Place[] | null> = new BehaviorSubject<Place[] | null>(null);
 
-  ];
+  constructor(private firestore: AngularFirestore) {
+    let getSubscription = this.getPlacesAPIObservable();
 
-  get places(){
-    return [...this._places]; //return a copy of the array in order to directly edit the places 
+    getSubscription.subscribe(data => {
+      this.allPlaces.next(data.map(e => {
+          return {
+            id: e.payload.doc.id,
+            ...(e.payload.doc.data() as {})
+          } as Place;
+        }));
+    });
   }
 
-  constructor() { }
+  getPlacesAPIObservable() {
+    return this.firestore.collection('places').snapshotChanges();
+  }
 
-  getPlace(id: string){
-    return {...this._places.find(p => p.id === id)}; //clone the entire object
+  getPlaceAPIObservable(id: string) {
+    return this.firestore.collection('places').doc(id).get();
+  }
+
+  createPlaceAPIPromise(place: Place) {
+    // Normalise model for Firebase
+    delete place.id;
+    place = {...place};
+    place.openDays = {...place.openDays};
+
+    return this.firestore.collection('places').add(place);
+  }
+
+  updatePlaceAPIPromise(place: Place) {
+    // Normalise model for Firebase
+    place = {...place};
+    place.openDays = {...place.openDays};
+
+    return this.firestore.doc('places/' + place.id).update(place);
+  }
+
+  deletePlace(place: Place) {
+    this.firestore.doc('places/' + place.id).delete();
   }
 }
